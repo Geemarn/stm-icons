edaca065f86d9f88bfc6658b784dca45082362ca
Adegoke <adeg4ife@gmail.com>
2021-06-14 17:06:23 +0100

    Updates

---
+++ precache-manifest.fc0b5bbd9db6144ab8651c9910065cd5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cec74e6bb9c24c93a23",
+    "url": "0.fb63f030.iframe.bundle.js"
+  },
+  {
+    "revision": "4d80ec9215febed79238",
+    "url": "4.48be7703.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.48be7703.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "422c6380296fd047d511",
+    "url": "5.9a6122b4.iframe.bundle.js"
+  },
+  {
+    "revision": "09a415d7d7e8e0d81750",
+    "url": "6.b0afbc39.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.b0afbc39.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2f3c0dad475f3420f8",
+    "url": "7.8bbaeacd.iframe.bundle.js"
+  },
+  {
+    "revision": "51d6e8f6595f763d8d5c5b8f17db1ab3",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "3378e070fa13781c2c2d",
+    "url": "main.1e77822f.iframe.bundle.js"
+  },
+  {
+    "revision": "af1e5f634ece5d5893c2",
+    "url": "runtime~main.cc90d05b.iframe.bundle.js"
+  },
+  {
+    "revision": "d43eb968e25e81ce0af629bad1069a19",
+    "url": "static/media/code-brackets.d43eb968.svg"
+  },
+  {
+    "revision": "8104e6f5a95cbe9c9615b47daf881fc7",
+    "url": "static/media/colors.8104e6f5.svg"
+  },
+  {
+    "revision": "3f9a1398f6372bd43806b239c70f4759",
+    "url": "static/media/comments.3f9a1398.svg"
+  },
+  {
+    "revision": "d3dc567bd3e26a031bfd7fab1e765508",
+    "url": "static/media/direction.d3dc567b.svg"
+  },
+  {
+    "revision": "e3812c4ff33e4e7ae9686a7fcfef393f",
+    "url": "static/media/flow.e3812c4f.svg"
+  },
+  {
+    "revision": "5ba97e9be21511d352f0056b1970ce21",
+    "url": "static/media/plugin.5ba97e9b.svg"
+  },
+  {
+    "revision": "3452b916238c37efec707e6979db3556",
+    "url": "static/media/repo.3452b916.svg"
+  },
+  {
+    "revision": "66f4b748553f89f290a3247a913072af",
+    "url": "static/media/stackalt.66f4b748.svg"
+  },
+  {
+    "revision": "3e41424b0737b4d067c9",
+    "url": "vendors~main.3400a828.iframe.bundle.js"
+  },
+  {
+    "revision": "25659aa5b361e026cba8b02c899cd6f0",
+    "url": "vendors~main.3400a828.iframe.bundle.js.LICENSE.txt"
+  }
+]);
